31ad1d99449c7c6421bb6f41681f37bb74cbbc71
njmaeys <nmaeys@hotmail.com>
2020-10-04 16:12:46 -0500

    more

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,7 +7,6 @@ const Results = ({ rollResult, diceModifier, rollPlusMod }) => {
     <div>
       <p>You rolled <b className="roll-result">{rollResult}</b> with a <b className="dice-modifier-result">{diceModifier}</b> modifier</p>
       <div className="total-roll-div">
-        <h2>Total Roll</h2>
         <h1 className="roll-plus-mod">{rollPlusMod}</h1>
       </div>
     </div>
